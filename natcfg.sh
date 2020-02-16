844f6f314975c65bc1a187dddb2475097cb5d16c
buste <32890006+wbc8615728@users.noreply.github.com>
2020-02-16 16:33:57 +0800

    新增nat类内网ip主机的本地ip设置

--- natcfg.sh
+++ natcfg.sh
@@ -44,9 +44,10 @@ service dnat start > /dev/null 2>&1
 
 
 ## 获取本机地址
-localIP=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 | grep -Ev '(^127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^10\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.1[6-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.2[0-9]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^172\.3[0-1]{1}[0-9]{0,1}\.[0-9]{1,3}\.[0-9]{1,3}$)|(^192\.168\.[0-9]{1,3}\.[0-9]{1,3}$)')
+localIP=""
 if [ "${localIP}" = "" ]; then
-        localIP=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1|head -n 1 )
+        echo -n "请输入本地ip:" ;read localIP
+        echo -n "本地ip为:${localIP}"
 fi
 
 rmIptablesNat(){
